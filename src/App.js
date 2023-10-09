f7b840da628f4353cbeeceb857018854096496f0
Muhammad <magomedov951@mail.ru>
2023-10-09 17:16:15 +0300

    testtt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1>Hello1</h1>
-        <h2>Test2</h2>
-        <h3>Test3</h3>
-      <h4>final test</h4>
-      <h4>final test 1asdasd</h4>
+      <h1>test</h1>
     </div>
   );
 }
